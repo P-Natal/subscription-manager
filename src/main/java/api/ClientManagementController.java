09eace027614a158e9be996fbe810b4958398dc9
Pedro Natal <pedro.natal@m4u.com.br>
2019-09-23 23:55:06 -0300

    Adding Client and Subscription controllers

---
+++ src/main/java/api/ClientManagementController.java
@@ -0,0 +1,11 @@
+package api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/client", produces = "application/json")
+public class ClientManagementController {
+
+
+}
---
+++ src/main/java/api/SubscriptionManagementController.java
@@ -0,0 +1,11 @@
+package api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/subscription", produces = "application/json")
+public class SubscriptionManagementController {
+
+
+}
